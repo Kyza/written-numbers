99bfcd233aa07297f8b2b7c94fea23daa6650055
Kyza <kyza@kyza.net>
2023-02-24 00:33:32 -0500

    No need for the macro or binding

--- written-numbers-wasm/src/lib.rs
+++ written-numbers-wasm/src/lib.rs
@@ -36,20 +36,6 @@ pub fn send_example_to_js() -> JsValue {
 }
 
 #[wasm_bindgen]
-extern "C" {
-	// Use `js_namespace` here to bind `console.log(..)` instead of just
-	// `log(..)`
-	#[wasm_bindgen(js_namespace = console)]
-	fn log(s: &str);
-}
-
-macro_rules! console_log {
-    // Note that this is using the `log` function imported above during
-    // `bare_bones`
-    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
-}
-
-#[wasm_bindgen]
 pub fn to_words(
 	number: &str,
 	options: JsValue,
